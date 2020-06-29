2e96849bdcc788a2abc8e62f9dfc49ec76cc7562
Marcus Aurelius <marcus+git@aurelius.org>
2020-06-29 11:09:45 -0700

    Added 'motd.sh', updated .py, updated screenshot.
    Wasn't super happy with the Python version of the MOTD script. Honestly
    feels like cheating to not use Bash. The one true language.
    
    Bash = Best.

--- motd.py
+++ motd.py
@@ -1,11 +1,17 @@
 #!/usr/bin/env python3
 # Using: http://patorjk.com/software/taag
 #
-# Requires editing /etc/pam.d/login:
-# `echo -e "session optional pam_exec.so stdout /path/to/motd" >> /etc/pam.d/login`
+# Paired with the 'update_motd.sh` script to pipe the output
+# from this script -> /etc/motd
+#
+# Arch Linux automatically reads /etc/motd on boot
 
-import re, os
+import re, os, sys
+from datetime   import timedelta
 from subprocess import check_output
+
+WIDTH = 80
+CENTER = round(WIDTH / 2)
 
 class Colors:
    # ANSI escape colors
@@ -23,7 +29,11 @@ class Colors:
    # 256 colors in the form of: \u001b[38;5;{color_number}m
    c = lambda x: f'\u001b[38;5;{x}m'
 
-   if os.environ.get('TERM') == 'linux':
+   CONSOLE_MODE = bool(len(sys.argv) > 1
+                   and sys.argv[-1] == 'linux'
+                    or os.environ.get('TERM') == 'linux')
+
+   if CONSOLE_MODE:
       FG     = brRed
       BG     = brBlack
       TEXT   = White
@@ -43,35 +53,41 @@ class Colors:
 #==============================================================================
 #                              senatus populusque
 #------------------------------------------------------------------------------
-print(f'''{Colors.FG}
-                   _                              _
-   ___ ___ ___ ___| |_ _ _ ___    ___ ___ ___ _ _| |_ _ ___ ___ _ _ ___
-  |_ -| -_|   | .'|  _| | |_ -|  | . | . | . | | | | | |_ -| . | | | -_|
-  |___|___|_|_|__,|_| |___|___|  |  _|___|  _|___|_|___|___|_  |___|___|
-                                 |_|     |_|                 |_|
-   {Colors.Reset}''')
+header_prefix = [
+   "┌─┐┌─┐┌┐┌┌─┐┌┬┐┬ ┬┌─┐  ┌─┐┌─┐┌─┐┬ ┬┬  ┬ ┬┌─┐┌─┐ ┬ ┬┌─┐",
+   "└─┐├┤ │││├─┤ │ │ │└─┐  ├─┘│ │├─┘│ ││  │ │└─┐│─┼┐│ │├┤ ",
+   "└─┘└─┘┘└┘┴ ┴ ┴ └─┘└─┘  ┴  └─┘┴  └─┘┴─┘└─┘└─┘└─┘└└─┘└─┘"]
+
+for line in header_prefix:
+   print(f'{Colors.FG}{line:^{WIDTH}}{Colors.Reset}')
 
 #==============================================================================
 #                                R O M A N U S
 #------------------------------------------------------------------------------
-ROMANUS = [
-   "      ██████╗  ██████╗ ███╗   ███╗ █████╗ ███╗   ██╗██╗   ██╗███████╗ ",
-   "      ██╔══██╗██╔═══██╗████╗ ████║██╔══██╗████╗  ██║██║   ██║██╔════╝ ",
-   "      ██████╔╝██║   ██║██╔████╔██║███████║██╔██╗ ██║██║   ██║███████╗ ",
-   "      ██╔══██╗██║   ██║██║╚██╔╝██║██╔══██║██║╚██╗██║██║   ██║╚════██║ ",
-   "      ██║  ██║╚██████╔╝██║ ╚═╝ ██║██║  ██║██║ ╚████║╚██████╔╝███████║ ",
-   "      ╚═╝  ╚═╝ ╚═════╝ ╚═╝     ╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝ ╚═════╝ ╚══════╝ \n"]
+header_main = [
+   "██████╗  ██████╗ ███╗   ███╗ █████╗ ███╗   ██╗██╗   ██╗███████╗",
+   "██╔══██╗██╔═══██╗████╗ ████║██╔══██╗████╗  ██║██║   ██║██╔════╝",
+   "██████╔╝██║   ██║██╔████╔██║███████║██╔██╗ ██║██║   ██║███████╗",
+   "██╔══██╗██║   ██║██║╚██╔╝██║██╔══██║██║╚██╗██║██║   ██║╚════██║",
+   "██║  ██║╚██████╔╝██║ ╚═╝ ██║██║  ██║██║ ╚████║╚██████╔╝███████║",
+   "╚═╝  ╚═╝ ╚═════╝ ╚═╝     ╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝ ╚═════╝ ╚══════╝"]
 
-for line in ROMANUS:
+# For some reason it isn't working to do a:
+# >>> print(f'{line:^{WIDTH}}')
+# Possibly because the line is actually about four-hundred characters long of
+# mostly ANSI color escapes. This is a little hacky, but it works.
+header_main_length = round(len(header_main[0]))
+width_difference = WIDTH - header_main_length
+compensate = round(width_difference / 2)
+
+for line in header_main:
    line = re.sub('█', f'{Colors.FG}█{Colors.Reset}', line)
    line = re.sub(r'([╗═╔╚║╝])', f'{Colors.BG}\\1{Colors.Reset}', line)
-   print(line)
+   print(f'{" " * compensate}{line}')
 
 #==============================================================================
 #                                    STATS
 #------------------------------------------------------------------------------
-spacer = ' '
-
 #-----| User |-----#
 user = os.environ.get('USER')
 
@@ -131,28 +147,21 @@ elif float_temp >= float_temp_crit:
    temp_color = Colors.CRIT
 
 #-----| Uptime |-----#
-_uptime = check_output(['uptime']).decode()
-_uptime = _uptime.split(',', 1)[0]
-uptime = _uptime.split('up')[1].strip()
+with open('/proc/uptime') as f:
+   _uptime = f.read()
 
-#-----| Upgradable |-----#
-_upgrade = check_output(['pacman', '-Qe']).decode()
-upgrade = len(_upgrade.split('\n'))
+_uptime = round(float(_uptime.split()[0]), 0)
+uptime = str(timedelta(seconds=_uptime))
 
 #==============================================================================
 #                                Format & Print
 #------------------------------------------------------------------------------
-# Spacer:
-print(f'{spacer * 12}{Colors.BG}{"─" * 50}{Colors.Reset}')
-print(f'{spacer * 24}    Welcome     {Colors.FG}{user}{Colors.Reset}')
+print(f'{Colors.BG}{"─"*50:^{WIDTH}}{Colors.Reset}')
+print(f'{"Welcome":>{CENTER-3}}  :  {Colors.FG}{user}{Colors.Reset}')
+print(f'{Colors.BG}{"─" * 27:^{WIDTH}}{Colors.Reset}')
 
-# Spacer:
-print(f'{spacer * 24}    {Colors.BG}────────────────────{Colors.Reset}')
-print(f'{spacer * 24}     Uptime     {Colors.ACCENT}{uptime}{Colors.Reset}')
-print(f'{spacer * 24}    IP Addr     {ip_addr}{Colors.Reset}')
-print(f'{spacer * 24}        Sys     {free_cpu} {temp_color}{temp} {Colors.BG}CPU{Colors.Reset}')
-print(f'{spacer * 24}                {free_mem} {Colors.BG}RAM{Colors.Reset}')
-print(f'{spacer * 24}                {Colors.TEXT}({upgrade}) {Colors.BG}packages{Colors.Reset}')
-
-# Spacer:
-print(f'{spacer * 12}{Colors.BG}{"─" * 50}{Colors.Reset}\n')
+print(f'{"Uptime":>{CENTER-3}}     {Colors.ACCENT}{uptime}{Colors.Reset}')
+print(f'{"IP Addr":>{CENTER-3}}     {ip_addr}')
+print(f'{"Sys":>{CENTER-3}}     {free_cpu} {temp_color}{temp} {Colors.BG}CPU{Colors.Reset}')
+print(f'{" ":>{CENTER-3}}     {free_mem} {Colors.BG}RAM{Colors.Reset}')
+print(f'{Colors.BG}{"─"*50:^{WIDTH}}{Colors.Reset}\n')
---
+++ motd.sh
@@ -0,0 +1,153 @@
+#!/bin/bash
+# MOTD w/ bash, awk, sed
+#
+# Paired with the 'update_motd.sh` script to pipe the output
+# from this script -> /etc/motd
+#
+# Arch Linux automatically reads /etc/motd on login
+
+WIDTH=80
+
+# ANSI escape colors
+Custom ()
+{
+   echo -e "\033[38;5;${1}m"
+}
+Black="\033[30m"   ;  brBlack="\033[30;1m"
+Red="\033[31m"     ;  brRed="\033[31;1m"
+Green="\033[32m"   ;  brGreen="\033[32;1m"
+Yellow="\033[33m"  ;  brYellow="\033[33;1m"
+Blue="\033[34m"    ;  brBlue="\033[34;1m"
+Magenta="\033[35m" ;  brMagenta="\033[35;1m"
+Cyan="\033[36m"    ;  brCyan="\033[36;1m"
+White="\033[37m"   ;  brWhite="\033[37;1m"
+
+Reset="\033[0m"
+
+# If we're in the console:
+if [[ "$TERM" == "linux" ]] || [[ "$1" == 'linux' ]] ; then
+   FG="${brRed}"
+   BG="${brBlack}"
+   TEXT="${White}"
+   ACCENT="${brYellow}"
+   GOOD="${brGreen}"
+   WARN="${brYellow}"
+   CRIT="${brRed}"
+else
+   FG="${Red}"
+   BG="${Black}"
+   TEXT="${Cyan}"
+   ACCENT="${Yellow}"
+   GOOD="${Green}"
+   WARN="${Yellow}"
+   CRIT="${Red}"
+fi
+
+#==============================================================================
+#                              senatus populusque
+#------------------------------------------------------------------------------
+declare -a HEADER_01=(
+   "┌─┐┌─┐┌┐┌┌─┐┌┬┐┬ ┬┌─┐  ┌─┐┌─┐┌─┐┬ ┬┬  ┬ ┬┌─┐┌─┐ ┬ ┬┌─┐"
+   "└─┐├┤ │││├─┤ │ │ │└─┐  ├─┘│ │├─┘│ ││  │ │└─┐│─┼┐│ │├┤ "
+   "└─┘└─┘┘└┘┴ ┴ ┴ └─┘└─┘  ┴  └─┘┴  └─┘┴─┘└─┘└─┘└─┘└└─┘└─┘")
+
+length_HEADER_O1=$( echo "$HEADER_01" | wc -m )
+offset_HEADER_01=$( echo "(${WIDTH} - $length_HEADER_O1) / 2" | bc )
+
+for line in "${HEADER_01[@]}" ; do
+   line="${FG}${line}${Reset}"
+   printf "%${offset_HEADER_01}s${line}\n"
+done
+
+
+#==============================================================================
+#                                R O M A N U S
+#------------------------------------------------------------------------------
+declare -a HEADER_02=(
+   "██████╗  ██████╗ ███╗   ███╗ █████╗ ███╗   ██╗██╗   ██╗███████╗"
+   "██╔══██╗██╔═══██╗████╗ ████║██╔══██╗████╗  ██║██║   ██║██╔════╝"
+   "██████╔╝██║   ██║██╔████╔██║███████║██╔██╗ ██║██║   ██║███████╗"
+   "██╔══██╗██║   ██║██║╚██╔╝██║██╔══██║██║╚██╗██║██║   ██║╚════██║"
+   "██║  ██║╚██████╔╝██║ ╚═╝ ██║██║  ██║██║ ╚████║╚██████╔╝███████║"
+   "╚═╝  ╚═╝ ╚═════╝ ╚═╝     ╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝ ╚═════╝ ╚══════╝")
+
+length_HEADER_O2=$( echo "$HEADER_02" | wc -m )
+offset_HEADER_02=$( echo "(${WIDTH} - $length_HEADER_O2) / 2" | bc )
+
+# Gotta use triple escape'd '\' for `sed`:  black="\\\033[30m", or "\\${Black}"
+for line in "${HEADER_02[@]}" ; do
+   fline=$( echo "$line" | sed -r "s/█/\\${FG}█\\${Reset}/g" )
+   fline=$( echo "$fline" | sed -r "s/([╗═╔╚║╝])/\\${BG}\1\\${Reset}/g" )
+   printf "%${offset_HEADER_02}s${fline}\n"
+done
+
+#==============================================================================
+#                                    STATS
+#------------------------------------------------------------------------------
+#-----| IP ADDR |-----#
+_IP_ADDR=$( ip -f inet -4 -br address | grep enp | awk '{print $3}' )
+
+_SEARCH="([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3})(\.[0-9]{1,3})(/[0-9]{1,2})"
+_REPLACE="`printf "${TEXT}"`\\1`printf "${ACCENT}"`\2`printf "${Reset}"``printf "${TEXT}"`\\3`printf "${Reset}"`"
+_IP_ADDR=$(echo "$_IP_ADDR" | sed -E "s:${_SEARCH}:${_REPLACE}:g")
+# Kinda a stupid way of going about this.
+# `sed` doesn't play nicely with '\e' or \033', only '\x1b'.
+
+#-----| RAM/CPU |-----#
+_RAM_FREE=$(free -mh | awk 'NR==2 { print $3 "/" $2 }')
+_CPU_FREE=$(top -n 1 | awk 'NR==3 { print $2 }' )
+
+#-----| Temperature |-----#
+_TEMP=$(sensors | grep -i -C1 "isa adapter")
+
+_TEMP_PRETTY=$( echo "$_TEMP" | awk 'NR==3 { print $4 }' )
+_TEMP_CURRENT=$( echo "$_TEMP" | awk 'NR==3 { print $4 }' | sed 's/[^0-9\.]//g')
+_TEMP_HIGH=$( echo "$_TEMP" | awk 'NR==3 { print $7 }' | sed 's/[^0-9\.]//g')
+_TEMP_CRIT=$( echo "$_TEMP" | awk 'NR==3 { print $10 }' | sed 's/[^0-9\.]//g')
+
+_IS_TEMP_GOOD=$(
+   awk "BEGIN { print (${_TEMP_CURRENT} < ${_TEMP_HIGH} ? \"True\" : \"\")}"
+)
+_IS_TEMP_HIGH=$(
+   awk "BEGIN { print (${_TEMP_CURRENT} >= ${_TEMP_HIGH} && ${_TEMP_CURRENT} < ${_TEMP_CRIT} ? \"True\" : \"\")}"
+)
+_IS_TEMP_CRIT=$(
+   awk "BEGIN { print (${_TEMP_CURRENT} >= ${_TEMP_CRIT} ? \"True\" : \"\")}"
+)
+
+[[ $_IS_TEMP_GOOD ]] && _TEMP_COLOR="${GOOD}"
+[[ $_IS_TEMP_HIGH ]] && _TEMP_COLOR="${WARN}"
+[[ $_IS_TEMP_CRIT ]] && _TEMP_COLOR="${CRIT}"
+
+#-----| Uptime |-----#
+_UPTIME=$( uptime | sed -E 's/.*up[ ]([0-9]+ days, )?[ ]?([0-9]+:[0-9]+).*/\1\2/g' )
+
+
+#==============================================================================
+#                                Format & Print
+#------------------------------------------------------------------------------
+_BIG_SPACER='──────────────────────────────────────────────────'
+_BIG_SPACER_length=$( echo $_BIG_SPACER | wc -m )
+_BIG_SPACER_offset=$( echo "($WIDTH - $_BIG_SPACER_length) / 2" | bc )
+
+_SMALL_SPACER='───────────────────────────'
+_SMALL_SPACER_length=$( echo $_SMALL_SPACER | wc -m )
+_SMALL_SPACER_offset=$( echo "($WIDTH - $_SMALL_SPACER_length) / 2" | bc )
+
+# To correctly format, use the following offset formula:
+# >>> midpoint = WIDTH / 2
+# >>> offset = midpoint - (len(word) + 3)
+#
+# In this case, we're printing for an 80column terminal, so midpoint is at 40
+# For example:
+# >>> word = "Welcome", length 7
+# >>> offset = 40 - (7 + 3) = 30
+
+printf "%${_BIG_SPACER_offset}s${BG}${_BIG_SPACER}${Reset}\n"
+printf "%30sWelcome  :  ${FG}${USER}${Reset}\n"
+printf "%${_SMALL_SPACER_offset}s${BG}${_SMALL_SPACER}${Reset}\n"
+printf "%31sUptime     ${ACCENT}${_UPTIME}${Reset}\n"
+printf "%30sIP Addr     ${_IP_ADDR}\n"
+printf "%34sSys     ${TEXT}${_CPU_FREE}%% ${_TEMP_COLOR}${_TEMP_PRETTY} ${BG}CPU${Reset}\n"
+printf "%37s     ${TEXT}${_RAM_FREE} ${BG}RAM${Reset}\n"
+printf "%${_BIG_SPACER_offset}s${BG}${_BIG_SPACER}${Reset}\n"
