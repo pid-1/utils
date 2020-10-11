8f7c24904d74f0391166be223e1ad1e2feed4ea5
Marcus Aurelius <marcus+git@aurelius.org>
2020-10-11 15:10:21 -0700

    Working completion.
    Working simple completion script for conf. Test symlinking to
    /etc/bash_completion.d/.
    
    Didn't feel like writing some complicated bash/sed JSON parsing script
    when I can just call Python.

--- completion/_conf.sh
+++ completion/_conf.sh
@@ -2,10 +2,31 @@
 # _conf.sh
 #
 # Completion script for `conf`
-# Parses the .json config file to pull out the nicknames.
+# Currently hard-codes the value for the data file. Should maybe want to make
+# dynamic in the future.
 
+#                                  functions
+#-------------------------------------------------------------------------------
 function _conf_completion ()
 {
+   [[ "${#COMP_WORDS[@]}" -ne 2 ]] && return
+
+   COMPREPLY=( $(compgen -W "${_FILES[@]}" -- "${COMP_WORDS[1]}") )
 }
 
+#                                  load data
+#-------------------------------------------------------------------------------
+# Loads names of files into an array
+declare -a _FILES=$(python -c "
+import json
+with open('$HOME/.config/hre-utils/dotfiles.json') as f:
+   try:
+      files = json.load(f)['Files'].keys()
+      print('\\n'.join(files))
+   except:
+      pass
+")
+
+#                                  complete
+#-------------------------------------------------------------------------------
 complete -F _conf_completion conf
